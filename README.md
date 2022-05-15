527402097b16c175d1c0e164381f36b855e45ef1
Sammy Muchiri <dev.miano@gmail.com>
2022-05-16 00:57:25 +0300

    created file and folder structure

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# Kurasa
+# Kurasa Blog App
+
+A web application that uses Python and Flask to help users create and share pitches for different scenarios
+
+## Author
+
+:link: [Sammy Muchiri](https://github.com/devmiano)
+
+## Installation / Setup instructions
+
+:point_right: Open Terminal {Ctrl+Alt+T}
+
+- `git clone https://github.com/devmiano/kurasa.git`
+
+- `cd kurasa` to enter the project folder
+
+- `code .` or `atom .` based on the text editor you have.
+
+- `chmod +x start.py && ./start.sh` to run the project in your terminal.
+
+## Testing the Application
+
+To run the tests for the class files:
+the command can be found commented out in the start file
+`python3 manage.py test`
+
+## BDD
+
+> The web application displays the recent blogs added by various users on the homepage.
+> Authors can sign up and write blogs in the application
+> The app also has links to the various blog categories on the navbar.
+> Users can be authenticated by signining with their email address and password
+> The user can comment on the blogs after login in
+> The user can view their comments on the blog after login in
+> Users and Authors can edit their own profile and add a profile picture to their profile
+
+## Technologies used
+
+- Python
+- Sass
+- Flask SQLALchemy
+- Flask Script
+- Flask Assets
+- PostgreSQL
+
+## License
+
+> MIT License :copyright: 2022 Sammy Muchiri.
+
+---
+
+## Collaborate
+
+To collaborate, reach me on :email: [Email](devmiano@gmail.com)
---
+++ app/models.py
@@ -0,0 +1,97 @@
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin, current_user
+from datetime import datetime
+from . import db
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(int(user_id))
+
+class User(UserMixin, db.Model):
+  __tablename__ = 'users'
+  
+  id = db.Column(db.Integer, primary_key=True)
+  first_name = db.Column(db.String(255))
+  last_name = db.Column(db.String(255))
+  username = db.Column(db.String(255), unique=True, index=True)
+  email = db.Column(db.String(255), unique=True, index=True)
+  bio = db.Column(db.String(255))
+  role = db.Column(db.String(255))
+  profile_pic_path = db.Column(db.String(255))
+  password_hash = db.Column(db.String(255))
+  blogs = db.relationship('Blog', backref='user', lazy='dynamic')
+  comments = db.relationship('Comment', backref='user', lazy='dynamic')
+  created = db.Column(db.DateTime,default=datetime.utcnow)
+  
+  @property
+  def password(self):
+    raise AttributeError('you cannot read the password')
+  
+  @password.setter
+  def password(self, password):
+    self.password_hash = generate_password_hash(password)
+    
+  def verify_password(self, password):
+    return check_password_hash(self.password_hash, password)
+  
+  def __repr__(self):
+      return f'User {self.username}'
+    
+class Category(db.Model):
+  __tablename__ = 'categories'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(255), unique=True, index=True)
+  title = db.Column(db.String(255))
+  
+  def __repr__(self):
+    return f'{self.name}'
+  
+class Role(db.Model):
+  __tablename__ = 'roles'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(255), unique=True, index=True)
+  title = db.Column(db.String(255))
+  
+  def __repr__(self):
+    return f'{self.name}'
+  
+class Blog(db.Model):
+  __tablename__ = 'blogs'
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(48))
+  short = db.Column(db.String(100))
+  blog_pic_path = db.Column(db.String(255))
+  content = db.Column(db.String(255), index = True)
+  category = db.Column(db.String(255), nullable=False)
+  author_id = db.Column(db.Integer,db.ForeignKey("users.id"), nullable = False)
+  comments = db.relationship('Comment', backref='blog', lazy='dynamic')
+  posted = db.Column(db.DateTime,default=datetime.utcnow)
+  
+  def save_blog(self):
+    db.session.add(self)
+    db.session.commit()
+  
+  @classmethod
+  def get_blogs(cls, id):
+    blogs = Blog.query.order_by(blog_id=id).desc().all()
+    return blogs
+    
+  
+  def __repr__(self):
+    return f'Pitch {self.caption}'
+  
+class Comment(db.Model):
+  __tablename__ = 'comments'
+  id = db.Column(db.Integer, primary_key=True)
+  caption = db.Column(db.String(255))
+  blog_id = db.Column(db.Integer, db.ForeignKey("blogs.id"), nullable= False)
+  author_id = db.Column(db.Integer ,db.ForeignKey("users.id"), nullable=False)
+  posted = db.Column(db.DateTime,default=datetime.utcnow)
+  
+  def save_comment(self):
+    db.session.add(self)
+    db.session.commit()
+  
+  def __repr__(self):
+    return f'{self.caption}'
