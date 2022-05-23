b10ebb60b41e59b2e90359a5a3da008fbc5630e4
Sammy Muchiri <dev.miano@gmail.com>
2022-05-23 04:06:44 +0300

    new schema change

--- app/models.py
+++ app/models.py
@@ -51,9 +51,9 @@ class Role(db.Model):
 class Blog(db.Model):
   __tablename__ = 'blogs'
   id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String(48), index = True)
+  title = db.Column(db.String(100), index = True)
   caption = db.Column(db.String(100))
-  content = db.Column(db.String(255))
+  content = db.Column(db.String(1000))
   blog_pic_path = db.Column(db.String(255))
   author_id = db.Column(db.Integer,db.ForeignKey("users.id"), nullable = False)
   comments = db.relationship('Comment', backref='blog', lazy='dynamic')
