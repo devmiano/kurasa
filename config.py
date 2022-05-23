1d5893d95f2062bdb6e25b53752b4def3e98e708
Sammy Muchiri <dev.miano@gmail.com>
2022-05-23 03:31:32 +0300

    db manage

--- config.py
+++ config.py
@@ -12,10 +12,10 @@ class Config:
   MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
   
 class ProdConfig(Config):
-  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-  if SQLALCHEMY_DATABASE_URI.startswith("postgres://"): 
-    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
-
+  # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+  # if SQLALCHEMY_DATABASE_URI.startswith("postgres://"): 
+  #   SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+  pass
 
 
 class DevConfig(Config):
